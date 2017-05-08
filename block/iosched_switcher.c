f17b8a6d1feceba1a3a146fce5b5b3d91736841b
Dorimanx <yuri@bynet.co.il>
2017-05-08 13:24:02 +0300

    block: I/O scheduler driver mod. delay setting noop when screen goes off.
    *Allow to delay setting noop when screen goes OFF. no point to set noop
    If user turn screen off and turn it On after 2sec to do something else!
    
    *Set dealy to 5sec, 10 is overkill.
    
    *Make sure the work is canceled ONLY if was actually active!
    
    (cherry picked from commit 9b15de0184ca41b2cff3a881fc5a849bcd680ba1)
    Signed-off-by: Enzaik <imernestodiaz@gmail.com>

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -19,46 +19,50 @@
 #include <linux/fb.h>
 
 #define NOOP_IOSCHED "noop"
-#define RESTORE_DELAY_MS (10000)
+#define RESTORE_DELAY_MS (5000)
 
-struct iosched_conf {
-	struct delayed_work restore_prev;
+struct req_queue_data {
+	struct list_head list;
 	struct request_queue *queue;
 	char prev_e[ELV_NAME_MAX];
 	bool using_noop;
 };
 
-static struct iosched_conf *config_g;
+static struct delayed_work restore_prev;
+static struct delayed_work sleep_sched;
+static DEFINE_SPINLOCK(init_lock);
+static struct req_queue_data req_queues = {
+	.list = LIST_HEAD_INIT(req_queues.list),
+};
 
-static void change_elevator(struct iosched_conf *c, bool use_noop)
+static void change_elevator(struct req_queue_data *r, bool use_noop)
 {
-	struct request_queue *q = c->queue;
-	char name[ELV_NAME_MAX];
+	struct request_queue *q = r->queue;
 
-	if (c->using_noop == use_noop)
+	if (r->using_noop == use_noop)
 		return;
 
-	c->using_noop = use_noop;
-
-	spin_lock_irq(q->queue_lock);
-	strcpy(name, q->elevator->type->elevator_name);
-	spin_unlock_irq(q->queue_lock);
+	r->using_noop = use_noop;
 
 	if (use_noop) {
-		if (strcmp(name, NOOP_IOSCHED)) {
-			strcpy(c->prev_e, name);
-			elevator_change(q, NOOP_IOSCHED);
-		}
+		strcpy(r->prev_e, q->elevator->type->elevator_name);
+		elevator_change(q, NOOP_IOSCHED);
 	} else {
-		if (!strcmp(name, NOOP_IOSCHED))
-			elevator_change(q, c->prev_e);
+		elevator_change(q, r->prev_e);
 	}
+}
+
+static void change_all_elevators(struct list_head *head, bool use_noop)
+{
+	struct req_queue_data *r;
+
+	list_for_each_entry(r, head, list)
+		change_elevator(r, use_noop);
 }
 
 static int fb_notifier_callback(struct notifier_block *nb,
 		unsigned long action, void *data)
 {
-	struct iosched_conf *c = config_g;
 	struct fb_event *evdata = data;
 	int *blank = evdata->data;
 
@@ -72,7 +76,9 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * Switch back from noop to the original iosched after a delay
 		 * when the screen is turned on.
 		 */
-		schedule_delayed_work(&c->restore_prev,
+		if (delayed_work_pending(&sleep_sched))
+			cancel_delayed_work_sync(&sleep_sched);
+		schedule_delayed_work(&restore_prev,
 				msecs_to_jiffies(RESTORE_DELAY_MS));
 		break;
 	default:
@@ -81,8 +87,10 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * the fb notifier chain call in case weird things can happen
 		 * when switching elevators while the screen is off.
 		 */
-		cancel_delayed_work_sync(&c->restore_prev);
-		change_elevator(c, true);
+		if (delayed_work_pending(&restore_prev))
+			cancel_delayed_work_sync(&restore_prev);
+		schedule_delayed_work(&sleep_sched,
+				msecs_to_jiffies(RESTORE_DELAY_MS));
 	}
 
 	return NOTIFY_OK;
@@ -94,36 +102,37 @@ static struct notifier_block fb_notifier_callback_nb = {
 
 static void restore_prev_fn(struct work_struct *work)
 {
-	struct iosched_conf *c = container_of(work, typeof(*c),
-						restore_prev.work);
+	change_all_elevators(&req_queues.list, false);
+}
 
-	change_elevator(c, false);
+static void set_sleep_sched_fn(struct work_struct *work)
+{
+	change_all_elevators(&req_queues.list, true);
 }
 
 int init_iosched_switcher(struct request_queue *q)
 {
-	struct iosched_conf *c;
+	struct req_queue_data *r;
 
-	if (!q) {
-		pr_err("Request queue is NULL!\n");
-		return -EINVAL;
-	}
-
-	if (config_g) {
-		pr_err("Already registered a request queue!\n");
-		return -EINVAL;
-	}
-
-	c = kzalloc(sizeof(*c), GFP_KERNEL);
-	if (!c)
+	r = kzalloc(sizeof(*r), GFP_KERNEL);
+	if (!r)
 		return -ENOMEM;
 
-	c->queue = q;
+	r->queue = q;
 
-	config_g = c;
+	spin_lock(&init_lock);
+	list_add(&r->list, &req_queues.list);
+	spin_unlock(&init_lock);
 
-	INIT_DELAYED_WORK(&c->restore_prev, restore_prev_fn);
+	return 0;
+}
+
+static int iosched_switcher_core_init(void)
+{
+	INIT_DELAYED_WORK(&restore_prev, restore_prev_fn);
+	INIT_DELAYED_WORK(&sleep_sched, set_sleep_sched_fn);
 	fb_register_client(&fb_notifier_callback_nb);
 
 	return 0;
 }
+late_initcall(iosched_switcher_core_init);
